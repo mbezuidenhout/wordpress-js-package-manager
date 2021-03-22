241798c263f49900f77e2f2f35ddcf7c5da165c9
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2021-03-22 12:56:37 +0200

    Initial import

---
+++ includes/class-js-package-manager-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://profiles.wordpress.org/mbezuidenhout/
+ * @since      1.0.0
+ *
+ * @package    Js_Package_Manager
+ * @subpackage Js_Package_Manager/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Js_Package_Manager
+ * @subpackage Js_Package_Manager/includes
+ * @author     Marius Bezuidenhout <marius dot bezuidenhout at gmail dot com>
+ */
+class Js_Package_Manager_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ js-package-manager.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://profiles.wordpress.org/mbezuidenhout/
+ * @since             1.0.0
+ * @package           Js_Package_Manager
+ *
+ * @wordpress-plugin
+ * Plugin Name:       JS Package Manager
+ * Plugin URI:        https://github.com/mbezuidenhout/wordpress-js-package-manager
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Tested up to:      5.7.0
+ * Author:            Marius Bezuidenhout
+ * Author URI:        https://profiles.wordpress.org/mbezuidenhout/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       js-package-manager
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'JS_PACKAGE_MANAGER_VERSION', '1.0.0' );
+
+/**
+ * The plugin file used in WordPress
+ */
+define( 'JS_PACKAGE_MANAGER_PLUGIN_FILE', __FILE__ );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-js-package-manager-activator.php
+ */
+function activate_js_package_manager() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-js-package-manager-activator.php';
+	Js_Package_Manager_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-js-package-manager-deactivator.php
+ */
+function deactivate_js_package_manager() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-js-package-manager-deactivator.php';
+	Js_Package_Manager_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_js_package_manager' );
+register_deactivation_hook( __FILE__, 'deactivate_js_package_manager' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-js-package-manager.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_js_package_manager() {
+
+	$plugin = new Js_Package_Manager();
+	$plugin->run();
+
+}
+run_js_package_manager();
